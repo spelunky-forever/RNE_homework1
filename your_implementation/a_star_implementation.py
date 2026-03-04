8f158637103afbf8c7a6a7c44a55971c22e8c25f
spelunky-forever <haydenpigko@gmail.com>
2026-03-04 23:26:48 +0800

    fix: a_star implement successfully.

--- your_implementation/a_star_implementation.py
+++ your_implementation/a_star_implementation.py
@@ -17,7 +17,6 @@ class AStarImplementation(AStarPlanner):
         self.g:dict[PathNode, float] = {}
         self.h:dict[PathNode, float] = {}
         self.visited_nodes:set[PathNode] = set()
-        self.visited_nodes.add(self.start_node)
         self.queue.add(self.start_node)
         self.g[self.start_node] = 0
         self.h[self.start_node] = calculate_node_distance(
@@ -42,25 +41,28 @@ class AStarImplementation(AStarPlanner):
         # to sample neighbor nodes, use 
         # self.get_neighbor_nodes(current_node)
 
+        if not self.priority_queue:
+            self.is_done.set()
+            return
+
         self.current_node = heapq.heappop(self.priority_queue)[2]
+        if self.current_node in self.visited_nodes:
+            return
+
         self.visited_nodes.add(self.current_node)
 
         if calculate_node_distance(self.current_node, self.goal_node) <= self.goal_threshold:
             if check_collision_free(self.occupancy_map, self.current_node, self.goal_node):
                 self.visited_nodes.add(self.goal_node)
+                self.goal_node.parent = self.current_node
+                self.goal_node.cost = self.g[self.current_node] + calculate_node_distance(self.current_node, self.goal_node)
                 self.is_done.set()
                 return
         
         self.neighbornood = self.get_neighbor_nodes(self.current_node)
         for neighbor in self.neighbornood:
             self.new_g = self.g[self.current_node] + calculate_node_distance(self.current_node, neighbor)
-            if neighbor in self.visited_nodes:
-                if self.new_g < self.g[neighbor]:
-                    self.g[neighbor] = self.new_g
-                    neighbor.parent = self.current_node
-                    neighbor.cost = self.new_g
-                    heapq.heappush(self.priority_queue, (self.g[neighbor] + self.h[neighbor], next(self.counter), neighbor))
-            else:
+            if self.new_g < self.g.get(neighbor, float('inf')):
                 self.g[neighbor] = self.new_g
                 self.h[neighbor] = calculate_node_distance(neighbor, self.goal_node)
                 neighbor.parent = self.current_node
